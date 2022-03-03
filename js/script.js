f42fe8f200b6cde885e42ff717b5e5cb80691fe3
gsashish <gsashishemail@gmail.com>
2022-03-03 13:01:40 +0530

    add files

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const idEl = document.getElementById("adviceId");
+const textEl = document.getElementById("adviceText");
+const changebtnEl = document.getElementById("change");
+let advice = { id: "", advice: "" };
+async function getAdvice() {
+  const url = "https://api.adviceslip.com/advice";
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  advice = data.slip;
+  console.log(advice);
+  // rendering advice
+  textEl.textContent = `"${advice.advice}"`;
+  idEl.textContent = advice.id;
+}
+// function that run when change btn is clicked
+function changeAdvice() {
+  getAdvice();
+}
+// Initaial function
+function init() {
+  getAdvice();
+  changebtnEl.addEventListener("click", changeAdvice);
+}
+init();
